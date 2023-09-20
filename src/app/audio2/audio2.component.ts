43adedb97de2ceb1a6e828c8797bf65969352fda
rgcreator <ricardovg35@gmail.com>
2023-09-20 11:12:45 +0200

    update

--- src/app/audio2/audio2.component.ts
+++ src/app/audio2/audio2.component.ts
@@ -18,31 +18,34 @@ ngOnInit(): void {
 let titlelement = document.getElementById("title") as HTMLElement;
 let nextsong = document.getElementById("new-song") as HTMLButtonElement;
 let pause = document.getElementById("pause") as HTMLButtonElement;
-Audiolist.push("https://github.com/rgcreator/Rodenttube/raw/main/Beverindeschulden.mp3");
-Audiolist.push("https://github.com/rgcreator/Rodenttube/raw/main/DitisRodenttube.mp3");
-Audiolist.push("https://github.com/rgcreator/Rodenttube/raw/main/Genietvanhetleven.mp3");
-Audiolist.push("https://github.com/rgcreator/Rodenttube/raw/main/Ikbeneeneenzamebever.mp3"); 
-Audiolist.push("https://github.com/rgcreator/Rodenttube/raw/main/Ikknaaghoutvoor.mp3");
-Audiolist.push("https://github.com/rgcreator/Rodenttube/raw/main/Kerstbever.mp3");
-Audiolist.push("https://github.com/rgcreator/Rodenttube/raw/main/Knagen.mp3");
-Audiolist.push("https://github.com/rgcreator/Rodenttube/raw/main/Knagenthout.mp3");
-Audiolist.push("https://github.com/rgcreator/Rodenttube/raw/main/NibbleAgain.mp3");
-Audiolist.push("https://github.com/rgcreator/Rodenttube/raw/main/Niemand%20kan%20beter%20knagen%20dan%20ik.mp3");
-Audiolist.push("https://github.com/rgcreator/Rodenttube/raw/main/OBeverburcht.mp3");
-Audiolist.push("https://github.com/rgcreator/Rodenttube/raw/main/RodenttubeRap.mp3");
-Audiolist.push("https://github.com/rgcreator/Rodenttube/raw/main/Uiteindelijkmaaktgeldniksuit.mp3");
-Audiolist.push("https://github.com/rgcreator/Rodenttube/raw/main/We%20gaan%20naar%20een%20bos.mp3");
-Audiolist.push("https://github.com/rgcreator/Rodenttube/raw/main/Wedden.mp3");
-Audiolist.push("https://github.com/rgcreator/Rodenttube/raw/main/Wood.mp3");
-Audiolist.push("https://github.com/rgcreator/Rodenttube/raw/main/Zoekdeconnectie.mp3");
-Audiolist.push("https://github.com/rgcreator/Rodenttube/raw/main/eetsla.mp3");
-
+Audiolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20muziek/Wedden.mp3?updatedAt=1695195512422");
+Audiolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20muziek/Zoek%20de%20connectie.mp3?updatedAt=1695195511349");
+Audiolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20muziek/We%20gaan%20naar%20een%20bos.mp3?updatedAt=1695195511384");
+Audiolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20muziek/Zoekdeconnectie.mp3?updatedAt=1695195509274");
+Audiolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20muziek/Niemand%20kan%20beter%20knagen%20dan%20ik.mp3?updatedAt=1695195508796");
+Audiolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20muziek/Genietvanhetleven.mp3?updatedAt=1695195508420");
+Audiolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20muziek/Wood.mp3?updatedAt=1695195507746");
+Audiolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20muziek/Wegaannaareenbos.mp3?updatedAt=1695195507694");
+Audiolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20muziek/Ikgaknagen.mp3?updatedAt=1695195505715");
+Audiolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20muziek/Uiteindelijkmaaktgeldniksuit.mp3?updatedAt=1695195504435");
+Audiolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20muziek/Uiteindelijk%20maakt%20geld%20niks%20uit.mp3?updatedAt=1695195503776");
+Audiolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20muziek/Niemandkanbeterknagendanik.mp3?updatedAt=1695195501297");
+Audiolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20muziek/Ikbeneeneenzamebever.mp3?updatedAt=1695195500986");
+Audiolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20muziek/NibbleAgain.mp3?updatedAt=1695195500588");
+Audiolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20muziek/RodenttubeRap.mp3?updatedAt=1695195499934");
+Audiolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20muziek/OBeverburcht.mp3?updatedAt=1695195499699");
+Audiolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20muziek/Knagenthout.mp3?updatedAt=1695195498606");
+Audiolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20muziek/Ikknaaghoutvoor.mp3?updatedAt=1695195496167");
+Audiolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20muziek/eetsla.mp3?updatedAt=1695195494562");
+Audiolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20muziek/Knagen.mp3?updatedAt=1695195494424");
+Audiolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20muziek/Beverindeschulden.mp3?updatedAt=1695195494422");
+Audiolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20muziek/DitisRodenttube.mp3?updatedAt=1695195494279");
+Audiolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20muziek/Kerstbever.mp3?updatedAt=1695195493830");
 title2 =  Audiolist[Math.floor(Math.random() *  Audiolist.length)];
 //Sets author of song
 //Gets random song
 audio.src =  title2.toString();
 //Gets title of song
-titlelement.innerHTML = title2.replace('https://github.com/rgcreator/Rodenttube/raw/main/', '').toString();
 audio.load();
 audio.play();
 
@@ -77,7 +80,6 @@ audio.src =  title2.toString();
 //Gets title of song
 
 //Sets title of song
-titlelement.innerHTML = titlelement.innerHTML = title2.replace('https://github.com/rgcreator/Rodenttube/raw/main/', '').toString();
 
 //Reload page if no songs are left
 if (Audiolist.length == 0){
--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -13,34 +13,36 @@ ngOnInit(): void {
   let titlelement = document.getElementById("title") as HTMLElement;
 let   playvideobutton = document.getElementById("playvideo") as HTMLButtonElement;
 let pausevideobutton = document.getElementById("pausevideo") as HTMLButtonElement;
+let iframe = document.getElementById("iframe") as HTMLIFrameElement;
 let audioenvideo = document.getElementById("video") as HTMLVideoElement;
   let videolist = new Array;
   let titlevideo = new String;
   let title2 = new String;
  let index = 0;
-videolist.push("https://github.com/rgcreator/Rodenttube/raw/main/Labelwilsamenwerken.mp4");
-videolist.push("https://github.com/rgcreator/Rodenttube/raw/main/WatkanjelerenvaneenInstagramvideovandaughtersdiary.webm");
-videolist.push("https://github.com/rgcreator/Rodenttube/raw/main/Onderhandelingenfnvenov.webm");
-videolist.push("https://github.com/rgcreator/Rodenttube/raw/main/BezigmeteenAlexaSkill.webm");
-videolist.push("https://github.com/rgcreator/Rodenttube/raw/main/DeERUvoertverbodinopolieuitRusland.webm");
-videolist.push("https://github.com/rgcreator/Rodenttube/raw/main/Deknaagdiercastkomtonline.webm");
-videolist.push("https://github.com/rgcreator/Rodenttube/raw/main/Dewaarheidoverdevluchtelingencrisis.webm")
-videolist.push("https://github.com/rgcreator/Rodenttube/raw/main/Geencompensatievoorkinderopvang.webm");
-videolist.push("https://github.com/rgcreator/Rodenttube/raw/main/Gokreclamesopalgemeenpubliekgaanverbodenworden.webm");
-videolist.push("https://github.com/rgcreator/Rodenttube/raw/main/HamsterAdajawordtgepest.webm");
-videolist.push("https://github.com/rgcreator/Rodenttube/raw/main/JakeDankwoordheeftgezegddathijhomois.webm");
-videolist.push("https://github.com/rgcreator/Rodenttube/raw/main/KlimaatprotestBeverwijk.webm");
-videolist.push("vhttps://github.com/rgcreator/Rodenttube/raw/main/KnaagdierlaatgrafsteenmakenvaninternetExplorer.webm");
+videolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20videos/Labelwilsamenwerken%20(2).webm?updatedAt=1695197060785");
+videolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20videos/GavanYTaf.webm?updatedAt=1695142495921");
+videolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20videos/WatkanjelerenvaneenInstagramvideovandaughtersdiary%20(1).webm?updatedAt=1695196926481");
+videolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20videos/ApplewilManchesterUnitedovernemenmeme.webm?updatedAt=1695142438327");
+videolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20videos/Onderhandelingenfnvenov.webm?updatedAt=1695142515837");
+videolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20videos/BezigmeteenAlexaSkill.webm?updatedAt=1695142494027");
+videolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20videos/DeERUvoertverbodinopolieuitRusland.webm?updatedAt=1695142441089");
+videolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20videos/Deknaagdiercastkomtonline.webm?updatedAt=1695197905737");
+videolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20videos/Dewaarheidoverdevluchtelingencrisis.webm?updatedAt=1695142453536")
+videolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20videos/Geencompensatievoorkinderopvang.webm?updatedAt=1695142452858");
+videolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20videos/Gokreclamesopalgemeenpubliekgaanverbodenworden.webm?updatedAt=1695142443035");
+videolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20videos/HamsterAdajawordtgepest.webm?updatedAt=1695142449610");
+videolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20videos/JakeDankwoordheeftgezegddathijhomois.webm?updatedAt=1695142457949");
+videolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20videos/KlimaatprotestBeverwijk.webm?updatedAt=1695142470736");
+videolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20videos/KnaagdierlaatgrafsteenmakenvaninternetExplorer.webm?updatedAt=1695142460077");
 videolist.push("https://github.com/rgcreator/Rodenttube/raw/main/MarkdeBeverheeftsms'jesverwijderd.webm");
 videolist.push("https://github.com/rgcreator/Rodenttube/raw/main/Nethoutgaatdelenduurdermaken.webm");
-videolist.push("video/RadiovijfdrieachtvoorprinsesMaximafoundation.webm");
-videolist.push("video/RodenttubestaatindePlayStore.webm");
-videolist.push("video/Siebertvanhoutenheeftfraudegepleegdmethoutkapjes.webm");
-videolist.push("video/StichtingdeknoevelinMaastricht.webm");
-videolist.push("video/Watalsdropshopreclameseerlijkzijn.webm");
-videolist.push("video/WatkanjelerenvaneenInstagramvideovandaughtersdiary.webm");
-videolist.push("video/Wijalsknaagdierenzeggennietéén.webm");
-videolist.push("video/Wijalsknaagdierenzeggenniettwee.webm");
+videolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20videos/RadiovijfdrieachtvoorprinsesMaximafoundation.webm?updatedAt=1695142516946");
+videolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20videos/RodenttubestaatindePlayStore%20(1).webm?updatedAt=1695198366764");
+videolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20videos/Siebertvanhoutenheeftfraudegepleegdmethoutkapjes.webm?updatedAt=1695142497725");
+videolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20videos/StichtingdeknoevelinMaastricht.webm?updatedAt=1695142515871");
+videolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20videos/Watalsdropshopreclameseerlijkzijn.webm?updatedAt=1695142518642");
+videolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20videos/Wijalsknaagdierenzeggenniet%C3%A9%C3%A9n.webm?updatedAt=1695142509094");
+videolist.push("https://ik.imagekit.io/fcknaagdier/Rodenttube%20videos/Wijalsknaagdierenzeggenniettwee.webm?updatedAt=1695142504945");
 var i = 0;
 let next = document.getElementById('volgendevideo') as HTMLButtonElement;
 let back = document.getElementById('vorigevideo') as HTMLButtonElement;
@@ -48,7 +50,6 @@ playvideobutton.onclick = () => {
   audioenvideo.src = videolist[i].toString();
   audioenvideo.load();
   audioenvideo.play();
-  titlelement.innerHTML = videolist[i].replace('https://github.com/rgcreator/Rodenttube/raw/main/', '').toString();
 }
 pausevideobutton.onclick  = function(){
 if (audioenvideo.paused == false){
